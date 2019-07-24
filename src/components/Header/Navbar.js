5bbf24e17d346989986806bb941f8995006be209
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-07-24 15:22:44 +0700

    [UPDATE] sidebar

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-import Sidebar from "./Sidebar";
-
 const Navs = styled.ul`
+position: fixed;
+margin-top: 0;
   background-color: #f47836;
   color: white;
   height: 60px;
@@ -15,45 +15,32 @@ const Icon = styled.i`
 const Logo = styled.img`
   height: 40px;
 `;
-class Navbar extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         toggled : true
-    //     }
-    // }
-    handleSidebar = async () => {
-        let toggled = ""
-        // if(this.state.toggled !== true){
-        //     toggled = true
-        // }
-        // await this.setState({
-        //     toggled: toggled
-        // })
-        this.props.handleSidebar(toggled)
 
-    }
+class Navbar extends React.Component {
+  handleSidebar = async () => {
+    this.props.handleSidebar("");
+  };
   render() {
     return (
-      <div>
-        <Navs className="nav d-flex justify-content-between">
-          <li
-            className="nav-item d-sm-none d-block p-3"
-            onClick={this.handleSidebar}
-          >
-            <Icon className="fa fa-bars" />
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <Logo src="/img/kmutt.png" />
-            </a>
-          </li>
-          <li className="nav-item d-sm-none d-block p-3">
-            <Icon className="fa fa-user" />
-          </li>
-        </Navs>
-        {/* <Sidebar sidebar={this.state.toggled}/> */}
-      </div>
+      <nav class="navbar">
+  <a class="navbar-brand" href="#">Navbar</a>
+</nav>
+      // <Navs className="nav d-flex justify-content-between">
+      //   <li
+      //     className="nav-item d-sm-none d-block p-3"
+      //     onClick={this.handleSidebar}
+      //   >
+      //     <Icon className="fa fa-bars" />
+      //   </li>
+      //   <li className="nav-item">
+      //     <a className="nav-link" href="#">
+      //       <Logo src="/img/kmutt.png" />
+      //     </a>
+      //   </li>
+      //   <li className="nav-item d-sm-none d-block p-3">
+      //     <Icon className="fa fa-user" />
+      //   </li>
+      // </Navs>
     );
   }
 }
