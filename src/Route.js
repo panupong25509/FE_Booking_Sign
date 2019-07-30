d616d633806402fab05db87c6cd21bbbf6821121
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-07-30 13:45:45 +0700

    [UPDATE] new structure

--- src/Route.js
+++ src/Route.js
@@ -1,13 +1,37 @@
 import React from "react";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
 
-import Index from './pages/index'
+import FullHeader from "./pages/FullHeader";
 
+//Pages
+import Login from "./pages/Auth";
+
+import Booking from "./pages/Booking";
+import History from "./pages/History";
+import Error from "./pages/Error";
+
+const Font = styled.div`
+  font-family: "Kanit", sans-serif;
+`;
+
+const Pages = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={History} />
+      <Route exact path="/booking" component={Booking} />
+      <Route exact path="/error/:status" component={Error} />
+      <Route component={Error} />
+    </Switch>
+  );
+};
 function AppRouter() {
   return (
-      <BrowserRouter>
-        <Index/>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Font>
+        <FullHeader LoginPage={Login} Route={Pages} />
+      </Font>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/FullHeader.js
@@ -0,0 +1,75 @@
+import React from "react";
+import cookie from "react-cookies";
+
+import Navbar from "../components/Header/Navbar";
+import Sidebar from "../components/Header/Sidebar";
+
+class FullHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      StatusSidebar: true, //open sidebar ex.
+      SidebarWidth: "col-5 col-md-2 col-sm-3 p-0", //width of sidbar ex.
+      PageWidth: "col-12 col-md-10 col-sm-9", //width of Page ex.
+      NavWidth: "col-12", //width of navbar ex.
+      NavHeight: 60 //height of navbar ex.
+    };
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    let statusSidebar = false;
+    if (window.innerWidth > 575) {
+      statusSidebar = true;
+    }
+    this.setState({
+      StatusSidebar: statusSidebar
+    });
+  };
+
+  handleSidebar = async () => {
+    let statusSidebar = false;
+    if (this.state.StatusSidebar === false) {
+      statusSidebar = true;
+    }
+    await this.setState({
+      StatusSidebar: statusSidebar
+    });
+  };
+
+  render() {
+    const LoginPage = this.props.LoginPage;
+    const Route = this.props.Route;
+    if (cookie.load("user") !== undefined) {
+      return (
+        <div>
+          <Navbar
+            handleSidebar={this.handleSidebar}
+            width={this.state.NavWidth}
+            height={this.state.NavHeight}
+          />
+          <div className="col-12" style={{ paddingTop: this.state.NavHeight }}>
+            <div className="row">
+              <div className={this.state.SidebarWidth + " p-0"}>
+                <Sidebar
+                  open={this.state.StatusSidebar}
+                  size={this.state.SidebarWidth}
+                />
+              </div>
+              <div className={this.state.PageWidth + " p-0"}>
+                <Route />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return <LoginPage />;
+  }
+}
+
+export default FullHeader;
