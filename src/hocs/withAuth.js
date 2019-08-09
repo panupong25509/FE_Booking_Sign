7808ed760a4e2458b2810cf2d651c33841d9bed1
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-08-09 17:25:20 +0700

    [UPDATE] check addmin can not open page for user

--- src/hocs/withAuth.js
+++ src/hocs/withAuth.js
@@ -7,8 +7,11 @@ const withAuth = (WrappedComponent, checkAdmin) => {
   return class ComponentWithAuth extends Component {
     render() {
       if (cookie.load("jwt") === undefined) return <Redirect to="/login" />;
-      if (!checkAdmin) return <WrappedComponent {...this.props} />;
       let token = jwt.decode(cookie.load("jwt"), "bookingsign");
+      if (!checkAdmin && token.Role !== "admin")
+        return <WrappedComponent {...this.props} />;
+      if (!checkAdmin && token.Role === "admin")
+        return <Redirect to="/admin" />;
       if (token.Role !== "admin") return <Redirect to="/login" />;
       return <WrappedComponent {...this.props} />;
     }
